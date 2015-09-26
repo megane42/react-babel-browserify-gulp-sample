39a731618e1dd17e19ea874a48f983a0b21b925f
megane42 <h.kzm.gm@gmail.com>
2015-09-26 05:26:47 -0400

    Using uglify in 'gulp browserify'.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp       = require('gulp');
 var browserify = require('browserify');
 var babelify   = require('babelify');
 var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
 var webserver  = require('gulp-webserver');
+var uglify     = require('gulp-uglify');
 
 gulp.task('browserify', function() {
   browserify('./src/main.jsx', { debug: true })
     .transform(babelify)
     .bundle()
-    .on("error", function (err) { console.log("Error : " + err.message); })
-    .pipe(source('app.js'))
+    .pipe(source('app.min.js'))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./app/'))
 });
 
